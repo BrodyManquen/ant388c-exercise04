16e67cb4e65611df29a0e90605e3fc89ae2bd88e
Brody W. Manquen <brodymanquen@gmail.com>
2024-02-14 15:23:36 -0600

    pre-class 02/14/2024 Exercise 4

--- exercise04.R
+++ exercise04.R
@@ -36,18 +36,33 @@ length(solution[[1]])
 
 ## Step 4
 ### play_wordle() function
+evaluate_guess <- function(guess, solution, letters_left){
+  letters_left <- setdiff(letters_left, guess[[1]]) #remove guessed letters from list
+  result <- case_match(test,
+                       str_extract(test, solution[[1]]) ~ "+", ## finds correctly placed letters, sets to "+"
+                       intersect(test, solution[[1]]) ~ "~",   ## finds incorrectly placed letters, sets to "~"
+                       .default = "-"                          ## finds letters *not* in solution, sets to "-"
+                      )
+  return(result)
+}
 play_wordle <- function(solution, valid_list, num_guesses=6){
   n_guess <- 1 #initialize guess count
-  print(paste0("You have ", num_guesses, " to guess a ", length(solution[[1]]), " letter word."))
-  letters_left <- LETTERS
-  print(paste0("Unguessed letters: ", letters_left))
-  guess <- str_split(toupper(readline(paste0("Enter guess number ", n_guess, ": "))), pattern="")
+  print(paste0("You have ", num_guesses, " to guess a ", length(solution[[1]]), " letter word.")) #initial statement
+  letters_left <- LETTERS # do not override by putting in while loop
+  while (n_guess <= num_guesses){ #while loop for guess counter
+    print(paste0("Unguessed letters: ", letters_left)) #print unguessed letters
+    guess <- readline(paste0("Enter guess number ", n_guess, ": ")) %>% toupper() %>% str_split(pattern="") #parse guess
+    evaluation <- evaluate_guess(guess, solution, letters_left)
+    n_guess <- n_guess + 1 #ticker
+  }
+
   return(guess)
-  #evaluate_guess <- function(guess, solution){
-    
-  #}
-  #while n_guess < num_guesses
+
 }
-test <- play_wordle(solution, valid_list)
-charmatch(solution[[1]], test[[1]])
-solution[[1]] %in% test[[1]]
+
+### Testing Grounds
+test_run <- play_wordle(solution, valid_list)
+test <- c("H", "O", "N", "E", "C")
+solution[[1]]
+###
+

bab21be37d4f387277249cec1c22b27d53edc21d
Brody W. Manquen <brodymanquen@gmail.com>
2024-02-18 17:29:47 -0600

    Finish Wordle

--- exercise04.R
+++ exercise04.R
@@ -1,9 +1,7 @@
 # Exercise 4 - Mock WORDLE
 library("tidyverse")
-install.packages("rlist")
-library(rlist)
+library("Hmisc")
 ## Step 1
-
 ### Load data with custom dictionary reader function
 scrabble_url <- "https://raw.githubusercontent.com/difiore/ada-2024-datasets/main/collins-scrabble-words-2019.txt"
 googleword_url <- "https://raw.githubusercontent.com/difiore/ada-2024-datasets/main/google-10000-english-usa-no-swears.txt"
@@ -31,22 +29,58 @@ pick_solution <- function(solution_list, word_length=5){
     str_split(pattern="")
   return(solution)
 }
+# Pick a test solution
 solution <- pick_solution(solution_list)
 length(solution[[1]])
 
 ## Step 4
 ### play_wordle() function
 evaluate_guess <- function(guess, solution){
-  result <- str_replace_all(guess[[1]], pattern=solution[[1]], replacement="*") %>%
-    case_match(
-      "*" ~ "*",
-      intersect(guess[[1]], solution[[1]]) ~ "+",   ## finds incorrectly placed letters, sets to "+"
-      .default = "-"                                ## letters *not* in solution, sets to "-"
+  word_length <- length(solution[[1]])
+  text_result <- rep("-", word_length)
+  # I've integrated your 'ugly hack' 
+  ## Calculate each letter counts
+  guess_count <- tibble(letter=guess[[1]]) %>%
+    group_by(letter) %>%
+    summarise(n_in_guess=n())
+  solution_count <- tibble(letter=solution[[1]]) %>%
+    group_by(letter) %>%
+    summarise(n_in_solution=n())
+  counts <- inner_join(
+    guess_count,
+    solution_count, by="letter") %>%
+    mutate(to_clear=n_in_guess-n_in_solution) %>%
+    filter(to_clear>0) %>%
+    select(letter, to_clear) #select the count of each letter in the guess that is "in excess" of the solution's count
+  #Create * + - output result w/o account for repeats
+  for (i in 1:word_length){
+    text_result[i] <-
+      case_when(
+        guess[[1]][i] %in% solution[[1]] & guess[[1]][i] == solution[[1]][i] ~ "*",
+        guess[[1]][i] %in% solution[[1]] & guess[[1]][i] != solution[[1]][i] ~ "+",
+        guess[[1]][i] %nin% solution[[1]] ~ "-"
       )
-  return(result)
+    # loop through each letter with excess counts, check against guess[[1]] input (except for ones correct (*))
+    for (j in counts$letter){
+      if (guess[[1]][i]==j & text_result[i] != "*" & counts[counts$letter==j,]$to_clear>0){
+        text_result[i] <- "-"
+        counts[counts$letter==j,]$to_clear <- counts[counts$letter==j,]$to_clear - 1
+      }
+    }
+  }
+  return(text_result)
+  ## Vestigial code that does not account for excess +
+  # result <- str_replace_all(guess[[1]], pattern=solution[[1]], replacement="*") %>%
+  #   case_match(
+  #     "*" ~ "*",
+  #     intersect(guess[[1]], solution[[1]]) ~ "+",   ## finds incorrectly placed letters, sets to "+"
+  #     .default = "-"                                ## letters *not* in solution, sets to "-"
+  #     )
+  # return(result)
 }
-play_wordle <- function(solution, valid_list, num_guesses=6){
+play_wordle <- function(solution, solution_list, num_guesses=6){
   library("tidyverse")
+  library("Hmisc")
   n_guess <- 1 #initialize guess count
   print(paste0("Welcome to my makeshift wordle! You have ", num_guesses, " to guess a ", length(solution[[1]]), " letter word.")) #initial statement
   print("* is correct placement, + is incorrect placement, and - is an incorrect letter.")
@@ -56,33 +90,35 @@ play_wordle <- function(solution, valid_list, num_guesses=6){
   while (n_guess <= num_guesses){ #while loop for guess counter
     print(paste0("Unguessed letters: ", letters_left)) #print unguessed letters
     guess <- readline(paste0("Enter guess number ", n_guess, ": ")) %>% toupper() %>% str_split(pattern="") #parse guess
-    if (length(guess[[1]])== word_length) {
+    if (length(guess[[1]])== word_length & paste(guess[[1]], collapse='') %in% solution_list) {
       letters_left <- setdiff(letters_left, guess[[1]]) #remove guessed letters from list
       evaluation <- evaluate_guess(guess, solution)
       print(evaluation)
       if (identical(guess[[1]], solution[[1]])) {
+        cat('\n')
         print(paste0("Correct! The solution is: ", paste(solution[[1]], collapse='')))
         break
       } else if (n_guess < num_guesses) {
-        print("Guess again!")
+        cat('\n')
+        print("Guess again!") #put some space around it to make sure you see the output!
+        cat('\n')
         n_guess <- n_guess + 1
       } else {
+        cat('\n')
         print("Incorrect!")
         print(paste0("The correct answer was: ", paste(solution[[1]], collapse='')))
         break
       }
 
     } else {
-      print(paste0("Entry invalid! Please enter a ", word_length, " letter word."))
+      cat('\n')
+      print(paste0("Entry invalid! Please enter a ", word_length, " letter word or a word in the game dictionary."))
+      cat('\n')
     }
   }
 }
-
-full_wordle <- function(solution_list, word_length=5, valid_list, num_guesses=6){
+full_wordle <- function(solution_list, word_length=5, num_guesses=6){
   solution <- pick_solution(solution_list)
-  game <- play_wordle(solution, valid_list, num_guesses=6)
+  game <- play_wordle(solution, solution_list, num_guesses=6)
 }
 full_wordle(solution_list, valid_list)
-### Testing Grounds
-test_run <- play_wordle(solution, valid_list)
-
